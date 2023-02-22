4b841018cf2598d97aaaa854436a844fdaf65174
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2023-02-22 12:57:13 +0530

    Add files via upload

---
+++ project/project/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+sys.path.append('/var/www/html/django/project')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+application = get_wsgi_application()
